39b6996906c7150cc624dbb4111d06064b029f26
Ameer <ameerapex496@gmail.com>
2023-12-11 08:33:45 +0100

    added comment , evetn lisnters and selected elements

--- src/script.js
+++ src/script.js
@@ -1,1 +1,26 @@
-console.log("hallo world");
+// selecting our elements
+var usersWrapper = document.getElementsByClassName("users-Wrapper")[0];
+var textInput = document.getElementsByClassName("text-Input")[0];
+// global varibal
+// api key
+// fetch api function
+// functions
+//eventlisnters
+window.addEventListener("load", function () {
+    console.log("hallo world");
+});
+textInput.addEventListener("input", function (e) {
+    console.log("you entered ", e.target.value);
+});
+// our html loop up
+// <div class="profile-Card">
+// <img
+//   class="profile-Img"
+//   src="https://randomuser.me/api/portraits/men/0.jpg"
+//   alt=""
+// />
+// <div class="info-Wrap">
+//   <h3>Ameer Ameen</h3>
+//   <p>Amsterdam, Netherlands</p>
+// </div>
+// </div>
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,32 @@
-console.log("hallo world");
+// selecting our elements
+const usersWrapper = document.getElementsByClassName("users-Wrapper")[0];
+const textInput = document.getElementsByClassName("text-Input")[0];
+// global varibal
+
+// api key
+
+// fetch api function
+
+// functions
+
+//eventlisnters
+window.addEventListener("load", () => {
+  console.log("hallo world");
+});
+textInput.addEventListener("input", (e) => {
+  console.log("you entered ", e.target.value);
+});
+
+// our html loop up
+
+// <div class="profile-Card">
+// <img
+//   class="profile-Img"
+//   src="https://randomuser.me/api/portraits/men/0.jpg"
+//   alt=""
+// />
+// <div class="info-Wrap">
+//   <h3>Ameer Ameen</h3>
+//   <p>Amsterdam, Netherlands</p>
+// </div>
+// </div>
